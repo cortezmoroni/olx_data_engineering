3d0f0e223d6244c7bf60df6f31adf1d3920681ec
moroni cortez <59509868+cortezmoroni@users.noreply.github.com>
2026-08-07 16:31:25 -0300

    Update gold_real_estate_metrics.py
    atualização gold

--- Notebooks/Gold_olx/gold_real_estate_metrics.py
+++ Notebooks/Gold_olx/gold_real_estate_metrics.py
@@ -1,6 +1,6 @@
 
 from pyspark.sql import functions as F
-from pyspark.sql.functions import col, when, coalesce, lit, round, current_timestamp
+
 
 #A camada Gold disponibiliza dados prontos para consumo analítico.
 #Nesta etapa são criados indicadores de negócio como:
@@ -12,33 +12,47 @@ from pyspark.sql.functions import col, when, coalesce, lit, round, current_times
 
 
 
-df_silver = spark.table("workspace.default.silver_real_estate_listings")
-df_gold = (df_silver
-.withColumn("preco_m2",
-            when((col("area_m2").isNotNull()) & (col("area_m2") > 0),
-                 round(col("preco") / col("area_m2"), 2))
-)
-.withColumn("categoria_preco",
-            when(col("preco") < 300000, "baixo")
-            .when(col("preco") < 700000, "medio")
-            .otherwise("alto")
-)
-.withColumn("total_comodos",
-            coalesce(col("quartos"), lit(0))
-            + coalesce(col("banheiros"), lit(0))
-)
-.withColumn("Possui_garagem",
-            when(col("garagens") > 0, "sim").otherwise("nao")
-)
-.withColumn("dt_carga",
-            current_timestamp()
-)
+df_silver = spark.table(
+    "workspace.default.silver_real_estate_listings"
 )
 
-(
+df_gold = (
+    df_silver
 
-df_gold.write\
-    .format("delta")\
-    .mode("overwrite")\
-    .saveAsTable("workspace.default.gold_real_estate_metrics")
+    .withColumn(
+        "preco_m2",
+        F.when(
+            (F.col("area_m2").isNotNull()) & (F.col("area_m2") > 0),
+            F.round(F.col("preco") / F.col("area_m2"), 2)
+        )
     )
+
+    .withColumn(
+        "categoria_preco",
+        F.when(F.col("preco") < 300000, "baixo")
+         .when(F.col("preco") < 700000, "medio")
+         .otherwise("alto")
+    )
+
+    .withColumn(
+        "total_comodos",
+        F.coalesce(F.col("quartos"), F.lit(0))
+        + F.coalesce(F.col("banheiros"), F.lit(0))
+    )
+
+    .withColumn(
+        "possui_garagem",
+        F.when(F.col("garagens") > 0, "sim")
+         .otherwise("nao")
+    )
+
+    .withColumn(
+        "dt_carga",
+        F.current_timestamp()
+    )
+)
+
+df_gold.write \
+    .format("delta") \
+    .mode("overwrite") \
+    .saveAsTable("workspace.default.gold_real_estate_metrics")
